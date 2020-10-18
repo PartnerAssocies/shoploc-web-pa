54a860ea06984f179b34236c5fdca452b5f02952
Clément Salvador <clementsalvador.isn@gmail.com>
2020-10-19 00:59:17 +0200

    fix(app.module):correction import hello service

--- shoploc-pa/src/app/app.module.ts
+++ shoploc-pa/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello/hello.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { HelloService } from './services/hello.serive';
+import { HelloService } from './services/hello.service';
 
 @NgModule({
   declarations: [
