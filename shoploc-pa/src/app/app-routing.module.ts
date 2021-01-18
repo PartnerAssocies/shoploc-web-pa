212c7a6d4cad5c60fa31a27a1a42de56b5b48bbe
Clément Salvador <clementsalvador.isn@gmail.com>
2021-01-18 09:17:59 +0100

    (fix) : dernier conflit

--- shoploc-pa/src/app/app-routing.module.ts
+++ shoploc-pa/src/app/app-routing.module.ts
@@ -20,11 +20,8 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent , canActivate: [AuthGuard], data: {expectedRole: 'NOT_LOGGED'} },
   { path: 'profil', component: ProfilComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_BOTH'}},
   { path: 'commercant-list', component:  CommercantListComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_CLIENT'}},
-<<<<<<< HEAD
   { path: 'commande-list', component:  CommandeListComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_CLIENT'}},
   { path: 'creation-commande-client', component:  CreationCommandeClientComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_CLIENT'}},
-=======
->>>>>>> dev
   { path: 'commercant-produit', component: ProduitCommercantComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_COMMERCANT'}},
   { path: 'add-commercant-produit', component: AjoutProduitCommercantComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_COMMERCANT'}}
 ];
