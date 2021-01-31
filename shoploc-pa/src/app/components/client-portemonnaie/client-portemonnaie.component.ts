ed9e71ad991d6e94ef996f589610c6b47aead710
Rémy Marfil <37613040+marfilr@users.noreply.github.com>
2021-01-31 23:40:36 +0100

    Df08 (#17)
    * (feat) : client add money front part
    
    * routes changeMoney & getSoldeClient implemented
    
    * (fix) : code smells df08
    
    Co-authored-by: marfil <remy.marfil.etu@univ-lille.fr>

--- shoploc-pa/src/app/components/client-portemonnaie/client-portemonnaie.component.ts
+++ shoploc-pa/src/app/components/client-portemonnaie/client-portemonnaie.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CurrentUser } from 'src/app/models/CurrentUser.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { PorteMonnaieService } from 'src/app/services/porteMonnaie.service';
 
 @Component({
   selector: 'app-client-portemonnaie',
@@ -9,12 +14,25 @@ export class ClientPortemonnaieComponent implements OnInit {
 
   showModal : boolean;
   active : boolean;
+  username : string;
+  soldeClient : number;
+  addMoneyForm: FormGroup;
+  public currentUser : CurrentUser;
 
-  constructor() { }
+  constructor(private porteMonnaieService : PorteMonnaieService,
+              private authService : AuthService,
+              private formBuilder: FormBuilder,
+              private _location : Location) { }
 
   ngOnInit(): void {
     this.showModal = false;
     this.active = false;
+    this.initForm();
+    this.username = this.authService.currentUserValue.username;
+    this.porteMonnaieService.getSoldeClient(this.username).subscribe(res => {
+      let solde = res;
+      this.soldeClient = solde['solde'];
+    });
   }
 
   openModal(){
@@ -22,14 +40,29 @@ export class ClientPortemonnaieComponent implements OnInit {
     this.active = true;  
   }
 
+  initForm(){
+    this.addMoneyForm = this.formBuilder.group({
+      montant : ['', [Validators.required]]
+    });
+  }
+
   hide(){
     this.showModal = false;
     this.active = false;
   }
 
-  validate(){
+  back(){
+    this._location.back();
+  }
+
+  onSubmitForm(){
     this.showModal = false;
     this.active = false;  
+    const formValue = this.addMoneyForm.value;
+    let money = formValue['montant'];
+    this.porteMonnaieService.changeMoney(this.username, money).subscribe(res => {
+      window.location.reload();
+    });
   }
 
 }
--- shoploc-pa/src/app/models/data/ClientData.model.ts
+++ shoploc-pa/src/app/models/data/ClientData.model.ts
@@ -1,3 +1,13 @@
 export class ClientData {
-    
+    constructor(
+        public username : string,
+        public password : string,
+        public lieu : string,
+        public role : string,
+        public nom : string,
+        public prenom : string,
+        public argent : number,
+        public estVfp : boolean,
+        public pointsFidelites : number
+    ){}
 }
