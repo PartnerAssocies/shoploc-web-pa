bd933621aa94860a85064f92041328a79e5412a1
Clément Salvador <clement.slvdr@gmail.com>
2021-03-07 19:50:52 +0100

    Fixdf17 : correction des bugs et ajout du paiement via l'app (#31)
    * (feat) : Ajout du paiement via l'application pour les commandes en direct
    
    * (feat) : refactor ! On utilise le même component pour la commande client et commerçant. Delete du component pour la commande commerçant
    
    * (feat) : modification du menu clik&collect du commercant pour rediriger vers le lecteur de carte
    
    * (fix) : -remove unused imports
    
    Co-authored-by: Clément Salvador <clementsalvador.isn@gmail.com>

--- shoploc-pa/src/app/components/navbar/navbar.component.ts
+++ shoploc-pa/src/app/components/navbar/navbar.component.ts
@@ -10,16 +10,15 @@ import { CurrentUser } from 'src/app/models/CurrentUser.model';
 })
 export class NavbarComponent implements OnInit {
 
-  public role : string;
-  public currentUser : CurrentUser;
+  public role: string;
+  public currentUser: CurrentUser;
 
   constructor(
     private router: Router,
     private authenticationService: AuthService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    //this.role = this.authenticationService.currentUserValue.role;
     this.role = this.authenticationService.currentUserValue.role;
   }
 
@@ -28,11 +27,11 @@ export class NavbarComponent implements OnInit {
    * CLIENT : 
    * COMMERCANT : 
    */
-  public navigateClickAndCollect(){
-    if(this.role == 'ROLE_CLIENT'){
+  public navigateClickAndCollect() {
+    if (this.role == 'ROLE_CLIENT') {
       this.router.navigate(['commande-list']);
     } else {
-      this.router.navigate(['commercant-achat-magasin']);
+      this.router.navigate(['lecteur-carte']);
     }
   }
 
@@ -40,7 +39,7 @@ export class NavbarComponent implements OnInit {
    * Redirige vers le menu de gestion des commerçant pour le profil admin
    * Uniquement pour le role ADMIN
    */
-  public navigateGestionCommercant(){
+  public navigateGestionCommercant() {
     this.router.navigate(['gestionCommercant']);
   }
 
@@ -49,7 +48,7 @@ export class NavbarComponent implements OnInit {
    * COMMERCANT : 
    * ADMIN : 
    */
-  public navigateStats(){
+  public navigateStats() {
 
   }
 
@@ -57,7 +56,7 @@ export class NavbarComponent implements OnInit {
    * Redirige vers le menu de gestion VFP Client
    * Uniquement pour le role CLIENT
    */
-  public navigateVFP(){
+  public navigateVFP() {
     this.router.navigate(['statut-vfp']);
   }
 
@@ -65,22 +64,22 @@ export class NavbarComponent implements OnInit {
    * Redirige vers la mire d'accueil
    * Pour tous les users
    */
-  public navigateHome(){
-    if(this.role == 'ROLE_COMMERCANT'){
+  public navigateHome() {
+    if (this.role == 'ROLE_COMMERCANT') {
       this.router.navigate(['commercant-home']);
-    } else if(this.role == 'ROLE_CLIENT'){
-      this.router.navigate(['commercant-list'],{queryParams: { fromHome: true}});
+    } else if (this.role == 'ROLE_CLIENT') {
+      this.router.navigate(['commercant-list'], { queryParams: { fromHome: true } });
     } else {
       this.router.navigate(['']);
 
     }
   }
-  
+
   /**
    * Redirige vers le menu de gestion du porte monnaie
    * Uniquement pour le role CLIENT
    */
-  public navigatePorteMonnaie(){
+  public navigatePorteMonnaie() {
     this.router.navigate(['client-portemonnaie']);
   }
 
@@ -88,7 +87,7 @@ export class NavbarComponent implements OnInit {
    * Redirige vers le menu de gestion des produits
    * Uniquement pour le role COMMERCANT
    */
-  public navigateGestionsProduits(){
+  public navigateGestionsProduits() {
     this.router.navigate(['commercant-produit']);
   }
 
@@ -96,7 +95,7 @@ export class NavbarComponent implements OnInit {
    * Redirige vers le menu de création de notifications
    * Uniquement pour le role ADMIN
    */
-  public navigateMenuNotification(){
+  public navigateMenuNotification() {
 
   }
 
@@ -104,7 +103,7 @@ export class NavbarComponent implements OnInit {
    * Redirige vers le profile
    * Pour tous les users
    */
-  public navigateMenuProfile(){
+  public navigateMenuProfile() {
     this.router.navigate(['profil']);
   }
 }
