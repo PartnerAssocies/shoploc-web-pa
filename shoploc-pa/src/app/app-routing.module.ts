2ad202142fc17eeefb752abf16d42f97b81bc7a9
DECLERCK <theo.declercks@hotmail.fr>
2021-03-22 09:58:36 +0100

    (fix): routes

--- shoploc-pa/src/app/app-routing.module.ts
+++ shoploc-pa/src/app/app-routing.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
   { path: 'bonus-vfp', component: ActivationBonusVfpComponent, canActivate: [AuthGuard], data: { expectedRole: 'ROLE_CLIENT' } },
   { path: 'bonus-vfp/parking', component: ParkingBonusComponent, canActivate: [AuthGuard], data: { expectedRole: 'ROLE_CLIENT' } },
   { path: 'map', component: ClientMapComponent, canActivate: [AuthGuard], data: { expectedRole: 'ROLE_CLIENT' } },
-  { path: 'clien-stats', component: ClientStatsComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_CLIENT'}},
+  { path: 'client-stats', component: ClientStatsComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_CLIENT'}},
   { path: 'commercant-stats', component: CommercantStatsComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_COMMERCANT'}},
   { path: 'asso-commercant-stats', component: AssocommerceStatsComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_COMMERCANT'}},
   { path: 'mairie-stats', component: MairieStatsComponent, canActivate: [AuthGuard], data: {expectedRole: 'ROLE_ADMIN'}},
